d5f0cc247f62fbd894223dc522478fe3e00c0304
Monu <30193075+yaggarwa@users.noreply.github.com>
2020-05-02 02:03:34 +0530

    changes in server.js and appspec scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/tamb2.0
 sudo npm install pm2@latest -g
-sudo npm install express -g
-sudo npm install socket.io -g
-sudo npm install path -g
+sudo npm install express 
+sudo npm install socket.io 
+sudo npm install path 
